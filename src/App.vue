b4f8eeefee7f2f105707c813fb22002bcf8daa52
arielglvz <arielglvz@gmail.com>
2024-11-18 14:57:05 +0800

    Added the topic from the 5th video tutorial

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,63 @@
 <template>
   <h1>{{ title }}</h1>
-  <!-- <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <p>welcome...</p>
+  <teleport to="#modals" v-if="showModal">
+    <!-- @close is the custom event -->
+    <Modal theme="" @close="toggleModal">
+      <!-- Slots -->
+        <template v-slot:links>
+          <a href="#">sign up now</a>
+          <a href="#">more info</a>
+        </template>
+        <h1>Ninja Giveaway!</h1>
+        <p>Grab your ninja swag for half price!</p>
+    </Modal>
+  </teleport>
+
+  <teleport to="#modals" v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <template v-slot:linkTwo>
+        <a href="#">Button 1</a>
+        <a href="#">Button 2</a>
+      </template>
+      <h1>Sign up to the newsletter</h1>
+      <p>For updates and promo codes!</p>
+    </Modal>
+  </teleport>
+
+  <button @click="toggleModal">open modal (alt)</button>
+  <button @click="toggleModalTwo">open modal</button>
+  <!-- Template Refs allows us to store a reference to a DOM element inside a variable -->
+  <!-- <input type="text" ref="name"> 
+  <button @click="handleClick">click me</button> -->
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue';
 
 export default {
   name: 'App',
+  components: { Modal },
   data() {
     return {
-      title: 'My First Vue App :)'
+      title: 'My First Vue App :)',
+      showModal: false,
+      showModalTwo: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
     }
   }
-  // components: {
-  //   HelloWorld
-  // }
 }
 </script>
 
 <style>
-#app {
+#app, #modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
